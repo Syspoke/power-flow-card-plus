4073c815a4b7359c4c07fc13515ce8b1cdf83eed
NeoGeo <91853839+Syspoke@users.noreply.github.com>
2026-02-06 18:23:53 +0100

    Fix formatting in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' , ios: "12"  } }],
+    ['@babel/preset-env', { targets: { node: 'current', ios: "12" } }],
     '@babel/preset-typescript',
   ],
 };
